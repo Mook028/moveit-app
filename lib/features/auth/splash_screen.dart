b0ffa921435a05cb78e1252c10a924a629573f04
Montatip3028 <6731503028@lamduan.mfu.ac.th>
2026-04-27 21:36:41 +0700

    feat: Refactor SplashScreen to use StatefulWidget and implement automatic navigation

--- lib/features/auth/splash_screen.dart
+++ lib/features/auth/splash_screen.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../core/constants/routes.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      context.go(Routes.login); // ไปหน้า login อัตโนมัติ
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: GestureDetector(
-        onTap: () {
-          context.go(Routes.login);
-        },
-        child: Center(child: Image.asset('assets/images/logo.png', width: 180)),
-      ),
+      body: Center(child: Image.asset('assets/images/logo.png', width: 180)),
     );
   }
 }
